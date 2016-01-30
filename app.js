81b0748748d65f21970d8d208bea184f0f3d35ac
Robert Cekay <RobertCekay@gmail.com>
2016-01-29 21:37:21 -0600

    heroku

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var React = require('react');
 var express = require('express');
 var path = require('path');
+var http = require('http');
 
 var routes = require('./routes/index');
 var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
@@ -12,6 +16,6 @@ app.engine('jsx', require('express-React-views').createEngine());
 app.get('/', routes);
 app.get('/user/create', routes);
 
-app.listen(8000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
